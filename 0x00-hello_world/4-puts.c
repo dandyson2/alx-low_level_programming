593d4f49bcbfa5c9b1fbae50941a41b688a0f2ce
dandyson2 <dandysonvictor@yahoo.com>
2023-02-16 17:04:22 -0800

    Hello, puts, Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-/**
-* main - main point
-* return:0
-*/
+ /**
+ * main - main point
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-       	return(0);
+       	return 0;
 }
