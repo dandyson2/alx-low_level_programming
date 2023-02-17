51fa4066b5a25308b82076f7bc6dfd4d6b8c3551
dandyson2 <dandysonvictor@yahoo.com>
2023-02-16 19:02:52 -0800

    Intel, Task 7

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,9 @@
+/**
+ * File: 101-quote.c
+ * Auth: Benjamin M Musyoki
+ */
+#include<unistd>
+/**
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * Return: Always 1.
+ * int main(void){}
