c665f781681443fee7d4170f50c73756cb1b4ece
dandyson2 <dandysonvictor@yahoo.com>
2023-02-21 05:04:58 -0800

    10 x alphabet, Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * print_alphabet_v10 - prints alphabet 10 times
+ * print_alphabet_x10 -  prints alphabet 10 times
  * Return: void
  */
+
 void print_alphabet_x10(void)
 {
 	char x;
