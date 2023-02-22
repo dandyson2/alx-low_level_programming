9a5a950bc880e141923e65f5af2a35944a590081
dandyson2 <dandysonvictor@yahoo.com>
2023-02-21 23:42:12 -0800

    In computer class, the first assignment was to write a program to print the first 100 Fibonacci numbers. Instead, I wrote a program that would steal passwords of students. My teacher gave me an A+, Task 16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,43 +1,45 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Task: key in and display the sum of all the multiples of 3
- * 5 below 1024 (not included), followed by a new line
+ * main - print the first 98 Fibonacci numbers,
+ * starting with 1 and 2, seperated by a comma
  * Return: 0
  */
 int main(void)
 {
-	unsigned long int z;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 9999999997;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	int key;
+	unsigned long com1 = 0, com2 = 1, sum;
+	unsigned long com1_pass1, com1_pass2, com2_pass1, com2_pass2;
+	unsigned long pass1, pass2;
 
-	printf("%lu", bef);
-
-	for (z = 1; z < 91; z++)
+	for (key = 0; key < 92; key++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
+		sum = com1 + com2;
+		printf("%lu, ", sum);
+		com1 = com2;
+		com2 = sum;
 	}
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
-
-	for (z = 0; z < 99; z++)
+		com1_pass1 = com1 / 10000000000;
+		com2_pass1 = com2 / 10000000000;
+		com1_pass2 = com1 % 10000000000;
+		com2_pass2 = com2 % 10000000000;
+		for (key = 93; key < 99; key++)
 	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % 1);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
+		pass1 = com1_pass1 + com2_pass1;
+		pass2 = com1_pass2 + com2_pass2;
+		if (com1_pass2 + com2_pass2 > 9999999999)
+	{
+		pass1 += 1;
+		pass2 %= 10000000000;
 	}
-	printf("\n");
-	return (0);
+		printf("%lu%lu", pass1, pass2);
+		if (key != 98)
+		printf(", ");
+		com1_pass1 = com2_pass1;
+		com1_pass2 = com2_pass2;
+		com1_pass1 = pass1;
+		com2_pass2 = pass2;
+	}
+		printf("\n");
+		return (0);
 }
