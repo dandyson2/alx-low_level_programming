ab00c896fcf951e65f2105f658408229e95b800c
dandyson2 <dandysonvictor@yahoo.com>
2023-02-23 11:03:31 -0800

    Fizz-Buzz, Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -20,7 +20,7 @@ for (x = 1; x <= 100; x++)
 if (x == 100)
 printf("%s", b);
 else if ((x % 3 == 0) && (x % 5 == 0))
-printf("%s ", a);
+printf("%s ", ab);
 else if (x % 5 == 0)
 printf("%s ", b);
 else
