06c5534cb1218c14fdac5481b600276297163b44
dandyson2 <dandysonvictor@yahoo.com>
2023-02-23 05:23:12 -0800

    I believe in numbers and signs, Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_most_numbers - print the most numbers
+ * print_most_numbers - print the most numbers from 0 - 9 &!=(2&4)
  * Return: 0 Always
  */
 
@@ -13,8 +13,8 @@ void print_most_numbers(void)
 	{
 		if (i != '2' && i != '4')
 		{
-			_putchar(i);
+		_putchar(i);
 		}
-			_putchar('\n');
+		_putchar('\n');
 	}
 }
