9f1210679d81f29d29ad863c0ae9f8e08ca31901
dandyson2 <dandysonvictor@yahoo.com>
2023-02-23 10:58:23 -0800

    Fizz-Buzz, Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ * multiples of three print Fizz
+ * multiples of five print Buzz
+ * multiples of both three and five print FizzBuzz
+ * Return: 0 Always
+ */
+int main(void)
+{
+int x;
+char a[] = "Fizz";
+char b[] = "Buzz";
+char cd[] = "FizzBuzz";
+
+for (x = 1; x <= 100; x++)
+{
+if (x == 100)
+printf("%s", b);
+else if ((x % 3 == 0) && (x % 5 == 0))
+printf("%s ", a);
+else if (x % 5 == 0)
+printf("%s ", b);
+else
+printf("%d ", x);
+}
+printf("\n");
+return (0);
+}
