15d822d67f26b87e0f4381ff020bf0e45040c5af
dandyson2 <dandysonvictor@yahoo.com>
2023-02-23 04:38:48 -0800

    The numbers speak for themselves, Task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_numbers - check for digit (0 to 9)
+ * print_numbers - print numbers 0 - 9
  * Return: 0
  */
 void print_numbers(void)
 {
 	int f;
 
-	for (f = 48; f <= 58; f++)
+	for (f = '48'; f <= '58'; f++)
 	{
-		_putchar(f);
+	_putchar(f);
 	}
 	_putchar('\n');
 }
