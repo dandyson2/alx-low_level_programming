c67de1fdf7d284656c42c28c7df493a5455fcaaa
dandyson2 <dandysonvictor@yahoo.com>
2023-02-23 04:09:04 -0800

    The numbers speak for themselves, Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "maim.h"
+
+/**
+ * print_numbers - check for digit (0 to 9)
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int f;
+
+	for (f >= 48; f <= 58; f++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
