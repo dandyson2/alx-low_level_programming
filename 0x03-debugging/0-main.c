1e7d9a34ac56e6a6baff763a7306f0ea7d67a90a
dandyson2 <dandysonvictor@yahoo.com>
2023-02-23 16:21:34 -0800

    Multiple mains, Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * main - tests function that prints if integer is positive or negative
  * Return: 0
@@ -8,7 +9,8 @@ int main(void)
 {
 	int i;
 
-	i = 0;
+	i = 98;
 	positive_or_negative(i);
+
 	return (0);
 }
