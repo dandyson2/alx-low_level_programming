65131939c6b253100a55f6884176239bcdd1d485
dandyson2 <dandysonvictor@yahoo.com>
2023-02-27 00:46:51 -0800

    98 Battery st., Task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  a function that takes a pointer
+ * to an int as parameter
+ * updates the value it points to to 98
+ * @q: input
+ * Return: q
+ */
+void reset_to_98(int *q)
+{
+	*q = 98;
+}
