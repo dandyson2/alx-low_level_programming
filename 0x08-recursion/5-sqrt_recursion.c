e322ff28d36ca1551bc0d7e31e3f30f8b0259e74
dandyson2 <dandysonvictor@yahoo.com>
2023-03-08 05:30:05 -0800

    Your subconscious is looking for the dreamer, Task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - Your subconscious is looking for the dreamer
+ * @n: input
+ * Return: sqrt recursion
+ */
+int _sqrt_recursion(int n)
+{
+	int f = 0;
+
+	if (f < 0)
+		return (-1);
+	else if
+	{
+		return (_evaluate(f, n));
+	}
+}
