19237fda14a23fd5bfc11261cfa10409afce37da
dandyson2 <dandysonvictor@yahoo.com>
2023-03-13 10:56:34 -0700

    He who is not courageous enough to take risks will accomplish nothing in life, Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,14 +3,16 @@
 #include <stdio.h>
 /**
  * str_concat - function that concatenates two strings
+ * NULL str is treated as an empty string
  * @s1: ptr to str
  * @s2: ptr to str
  * Return: ptr to newly allocated memory
  * contains s1, s2 and null byte
+ * NULL on failure
  */
 char *str_concat(char *s1, char *s2)
 {
-	unsigned int max1, max2, size, y, z;
+	unsigned int max1, max2 size, y, z;
 	char *rstr;
 
 	if (s1 == NULL)
@@ -26,6 +28,8 @@ char *str_concat(char *s1, char *s2)
 	max2 = 0;
 	while (s2[max2] != '\0')
 		max2++;
+
+	size = max1 + max2;
 
 	rstr = malloc((sizeof(char) * size) + 1);
 	if (rstr == NULL)
