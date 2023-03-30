65511c8e5632ff44811cb5034e17b936517f78b0
dandyson2 <dandysonvictor@yahoo.com>
2023-03-30 07:13:56 -0700

    Print List, Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,9 +7,9 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t = _print;
+	size_t = nelem;
 
-	_print = 0;
+	nelem = 0;
 	while (h != NULL)
 	{
 		if (h->str == NULL)
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
-		_print++;
+		nelem++;
 	}
-	return (_print);
+	return (nelem);
 }
