9e5185f3c4d95392a334caa3c9c49ae630d332c2
dandyson2 <dandysonvictor@yahoo.com>
2023-03-30 15:34:20 -0700

    The Hare and the Tortoise, Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * first - function executed before main
- * Return: no return.
+ * display - function executed before main
+ * Return: void.
  */
 
-void __attribute__ ((constructor)) first()
+void __attribute__ ((constructor)) display()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
