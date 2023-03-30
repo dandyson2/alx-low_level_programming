1b0f21edd302622285d6ed4d535d82530d3969e2
dandyson2 <dandysonvictor@yahoo.com>
2023-03-30 15:18:47 -0700

    The Hare and the Tortoise, Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print - function that prints complex sentence
+ * Return: void
+ */
+
+void complex_sen ((constructor)) print()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
