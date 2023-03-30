9a40523ad609fad3be7fb8953169d73fdfa3dd51
dandyson2 <dandysonvictor@yahoo.com>
2023-03-30 15:56:34 -0700

    The Hare and the Tortoise, Task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: void.
  */
 
-void _attribute_ ((constructor)) print()
+void __attribute__ ((constructor)) print()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
