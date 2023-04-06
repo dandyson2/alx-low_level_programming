595651a0662abb05f0b5523745a1f1ddb6520619
dandyson2 <dandysonvictor@yahoo.com>
2023-04-06 11:59:46 -0700

    Zero, Task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "bit.h"
+
+/**
+ * binary_to_uint - function that converts a binary
+ * number to an unsigned int
+ * @b: The binary
+ * Return: unsigned integer
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int define;
+	int _unit, max;
+
+	if (!b)
+		return (0);
+
+	_unit = 0;
+
+	for (_unit = 0; b[_unit] != '\0'; _unit++)
+		;
+
+	for (_unit--, max = 1; _unit >= 0; _unit--, max *= 2)
+	{
+		if (b[_unit] != '0' && b[_unit] != '1')
+		{
+			return (0);
+		}
+		if (b[_unit] & 1)
+		{
+			define += max;
+		}
+	}
+	return (define);
+}
+
