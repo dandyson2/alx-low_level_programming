c1081c103b61431760fb8538908e17b5e92cf0e0
dandyson2 <dandysonvictor@yahoo.com>
2023-07-07 17:26:25 -0700

    Hash table add, Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,43 +13,43 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
-return (0);
+	hash_node_t *new;
+	char *value_copy;
+	unsigned long int index, i;
 
-char *value_copy = strdup(value);
-if (value_copy == NULL)
-return (0);
+	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+		return (0);
 
-unsigned long int index = key_index((const unsigned char *)key, ht->size);
-unsigned long int i = index;
+	value_copy = strdup(value);
+	if (value_copy == NULL)
+		return (0);
 
-while (ht->array[i] && strcmp(ht->array[i]->key, key) != 0)
-i++;
+	index = key_index((const unsigned char *)key, ht->size);
+	for (i = index; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
 
-if (ht->array[i])
-{
-free(ht->array[i]->value);
-ht->array[i]->value = value_copy;
-return (1);
-}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
 
-hash_node_t *new = malloc(sizeof(hash_node_t));
-if (!new)
-{
-free(value_copy);
-return (0);
-}
-
-new->key = strdup(key);
-if (!new->key)
-{
-free(new);
-return (0);
-}
-
-new->value = value_copy;
-new->next = ht->array[index];
-ht->array[index] = new;
-
-return (1);
+	return (1);
 }
