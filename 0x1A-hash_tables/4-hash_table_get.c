55f77dbc246792f0936fd44837c00a2de291f0ef
dandyson2 <dandysonvictor@yahoo.com>
2023-07-07 21:29:04 -0700

    Hash table Betty, Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,21 +8,22 @@
 * Return: Value associated with element, NULL if
 * key would not be found
 */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-hash_node_t *node;
-unsigned long int index;
+	hash_node_t *node;
+	unsigned long int index;
 
-if (ht == NULL || key == NULL || *key == '\0')
-return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-index = key_index((const unsigned char *)key, ht->size);
-if (index >= ht->size)
-return (NULL);
+	index = key_index((const unsigned char *)key, ht->size);
+	if (index >= ht->size)
+		return (NULL);
 
-node = ht->array[index];
-while (node && strcmp(node->key, key) != 0)
-node = node->next;
+	node = ht->array[index];
+	while (node && strcmp(node->key, key) != 0)
+		node = node->next;
 
-return ((node == NULL) ? NULL : node->value);
+	return ((node == NULL) ? NULL : node->value);
 }
