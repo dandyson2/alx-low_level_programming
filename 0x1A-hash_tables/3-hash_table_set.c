5ad1774defefc9121b55b749c584fef66347c4d2
dandyson2 <dandysonvictor@yahoo.com>
2023-07-07 17:07:20 -0700

    Hash table, add, Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,51 @@
+#include "hash_tables.h"
+
+/**
+* hash_table_set - Function that add an element to hash table
+* @ht: The hash table
+* @key: The key, cannot be an empty string
+* @value: The value
+* Return: 1 if succeeded, otherwise 0.
+*/
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
+return (0);
+
+char *value_copy = strdup(value);
+if (value_copy == NULL)
+return (0);
+
+unsigned long int index = key_index((const unsigned char *)key, ht->size);
+unsigned long int i = index;
+
+while (ht->array[i] != NULL)
+{
+if (strcmp(ht->array[i]->key, key) == 0)
+{
+free(ht->array[i]->value);
+ht->array[i]->value = value_copy;
+return (1);
+}
+i++;
+}
+
+hash_node_t *new = malloc(sizeof(hash_node_t));
+if (new == NULL)
+{
+free(value_copy);
+return (0);
+}
+new->key = strdup(key);
+if (new->key == NULL)
+{
+free(new);
+return (0);
+}
+new->value = value_copy;
+new->next = ht->array[index];
+ht->array[index] = new;
+
+return (1);
+}
