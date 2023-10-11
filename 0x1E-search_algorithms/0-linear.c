499479e75aa498f81158418f0b2f002e8e222a7e
dandyson2 <dandysonvictor@yahoo.com>
2023-10-11 13:20:14 -0700

    Linear search, Task 0

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -14,18 +14,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
+if (!array || size == 0)
+return (-1);
 
-	if (!array || size == 0)
-		return (-1);
+for (size_t i = 0; i < size; i++)
+{
+printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+if (array[i] == value)
+return (i);
+}
 
-	while (i < size)
-	{
-		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-		i++;
-	}
-
-	return (-1);
+return (-1);
 }
