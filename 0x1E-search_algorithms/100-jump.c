1d339b7b1b6a4e53473941715c0c26c4eaae394c
dandyson2 <dandysonvictor@yahoo.com>
2023-10-11 14:02:14 -0700

    Jump search, Task 7

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -8,42 +8,33 @@
 * @size: size of the array
 * @value: value to look for
 *
-* Return: index of the value found, or -1
+* Return: index of the value found, or -1 if not found
 */
+
 int jump_search(int *array, size_t size, int value)
 {
 if (!array || size == 0)
 return (-1);
 
 size_t step = sqrt(size);
-size_t start, end;
+size_t prev = 0;
+size_t curr = 0;
 
-for (start = 0, end = step; start < size; start = end, end = start + step)
+while (curr < size && array[curr] < value)
 {
-printf("Value checked array[%lu] = [%d]\n", start, array[start]);
-if (end < size)
-{
-if (array[start] <= value && value <= array[end])
-{
-printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-break;
-}
-}
-else
-{
-if (array[start] <= value)
-{
-printf("Value found between indexes [%lu] and [%lu]\n", start, end);
-break;
-}
-}
+printf("Value checked array[%lu] = [%d]\n", curr, array[curr]);
+prev = curr;
+curr += step;
 }
 
-for (size_t i = start; i <= end && i < size; ++i)
+printf("Value found between indexes [%lu] and [%lu]\n", prev, curr);
+
+while (prev <= curr && prev < size)
 {
-printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-if (array[i] == value)
-return (i);
+printf("Value checked array[%lu] = [%d]\n", prev, array[prev]);
+if (array[prev] == value)
+return (prev);
+prev++;
 }
 
 return (-1);
